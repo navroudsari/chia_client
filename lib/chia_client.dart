955273b56aed0c6f10bb57da204982a536da9278
Nav Roudsari <nav@roudsari.io>
2022-06-07 10:52:38 +0100

    [wip] - expose rpc client

--- lib/chia_client.dart
+++ lib/chia_client.dart
@@ -1,5 +1,6 @@
 library chia_client;
 
+export 'package:chia_client/src/client/rpc_client.dart';
 export 'package:chia_client/src/model/chia_models.dart';
 export 'package:chia_client/src/model/rpc_models.dart';
 export 'package:chia_client/src/services/full_node/full_node_service.dart';
